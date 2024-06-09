e06d0dfdd6a750a68e65aa71b97be91e0e96a593
Herman Murauyou <herman.acc@proton.me>
2024-06-09 16:29:10 +0300

    transaltions added.

--- src/components/ItemPage.tsx
+++ src/components/ItemPage.tsx
@@ -16,13 +16,14 @@ interface ItemData {
     composition: string;
     supplements: string;
     vitamins: string;
+    food_value: string;
     energy_value: string;
     recommendation: string;
     country_id: string;
-    price_byn?: number | null;
-    net_weight?: string | null;
-    shelf_life?: string | null;
-    expiration_date?: string | null;
+    price_byn: number | null;
+    net_weight: string | null;
+    shelf_life: string | null;
+    expiration_date: string | null;
 }
 
 // const s3 = new AWS.S3({
@@ -40,6 +41,18 @@ export const ItemPage = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     const [t] = useTranslation("global")
+    const translatedName = t(`products.products_info.${itemData?.id}.name`);
+    const translatedDescription = t(`products.products_info.${itemData?.id}.description`);
+    const translatedComposition = t(`products.products_info.${itemData?.id}.composition`);
+    const translatedSupplements = t(`products.products_info.${itemData?.id}.supplements`);
+    const translatedFoodVaue = t(`products.products_info.${itemData?.id}.food_value`);
+    const translatedVitamins = t(`products.products_info.${itemData?.id}.vitamins`);
+    const translatedEnergyValue = t(`products.products_info.${itemData?.id}.energy_value`);
+    const translatedCountry = t(`products.products_info.${itemData?.id}.country_id`);
+    const translatedRecommendation = t(`products.products_info.${itemData?.id}.recommendation`);
+    const translatedShelfLife = t(`products.products_info.${itemData?.id}.shelf_life`);
+    const translatedExpirationDate = t(`products.products_info.${itemData?.id}.expiration_date`);
+    const translatedNetWeight = t(`products.products_info.${itemData?.id}.net_weight`);
     // const translatedName = t(`products.products_info.${itemData?.id}.name`);
     // const translatedDescription = t(`products.products_info.${itemData?.id}.description`);
     // const translatedCountry = t(`products.products_info.${itemData?.id}.country_id`);
@@ -157,18 +170,32 @@ export const ItemPage = () => {
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title"><span className={styles.name}>{itemData.name}</span></h5>
-                            <p className="card-text"><b className={styles.data}>{t("products.description")}:</b> {itemData.description}</p>
-                            <p className="card-text"><b className={styles.data}>Состав:</b> {itemData.composition}</p>
-                            <p className="card-text"><b className={styles.data}>Добавки:</b> {itemData.supplements}</p>
-                            <p className="card-text"><b className={styles.data}>Энергетическая ценность:</b> {itemData.energy_value}</p>
-                            <p className='card-text'><b className={styles.data}>{t("products.country")}:</b> {itemData.country_id}</p>
-                            <p className='card-text'><b className={styles.data}>Срок хранения:</b> {itemData.shelf_life}</p>
-                            <p className='card-text'><b className={styles.data}>Рекомендация:</b> {itemData.recommendation}</p>
-                            <p className='card-text'><b className={styles.data}>{t("products.shelf_life")}:</b> {itemData.expiration_date}</p>
-                            <p className='card-text'><b className={styles.data}>{t("products.net_weight")}:</b> {itemData.net_weight}</p>
+                            <div>
+                                <h5 className="card-title"><span className={styles.name}>{translatedName}</span></h5>
+                            </div>
+                            <div>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.description")}: </b>{translatedDescription}</p>
+                            </div>
+                            <div>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.composition")}: </b>{translatedComposition}</p>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.vitamins")}: </b>{translatedVitamins}</p>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.supplements")}: </b>{translatedSupplements}</p>
+                            </div>
+                            <div>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.energy_value")}: </b>{translatedEnergyValue}</p>
+                                <p className="card-text"><b className={styles.data}>{t("item_page.food_value")}: </b>{translatedFoodVaue}</p>
+                            </div>
+                            <div>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.shelf_life")}: </b>{translatedShelfLife}</p>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.recommendation")}: </b>{translatedRecommendation}</p>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.expiration_date")}: </b>{translatedExpirationDate}</p>
+                            </div>
+                            <div>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.country")}: </b>{translatedCountry}</p>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.net_weight")}: </b>{translatedNetWeight}</p>
+                            </div>
                             <div className="price-container">
-                                <p className='card-text'><b className={styles.data}>{t("products.price")}:</b> <span className={styles.price}>{itemData.price_byn} BYN</span></p>
+                                <p className='card-text'><b className={styles.data}>{t("item_page.price")}: </b><span className={styles.price}>{itemData.price_byn} BYN</span></p>
                             </div>
                         </div>
                     </div>
